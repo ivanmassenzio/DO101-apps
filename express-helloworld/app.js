3affdf3d9bd0ae44b23512ef956cb3d61a2489d3
ivan <ivanmassenzio@gmail.comgit config --global user.name Ivan Massenzio>
2020-05-16 08:37:14 -0300

    update para que fucione en os

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -9,7 +9,7 @@ app.get('/mars', function (req, res) {
   res.send('Hello Mars!\n');
 });
 
-app.listen(8100, function () {
-  console.log('Example app listening on port 8100!');
+app.listen(8080, function () {
+  console.log('Example app listening on port 8080!');
 });
 
